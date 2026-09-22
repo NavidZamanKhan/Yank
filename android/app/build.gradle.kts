81067fde3c35d8f7a39728a3d79ae777a03e75a8
Navid Zaman Khan <navid166418@gmail.com>
2026-09-22 22:49:40 +0600

    feat: add android system share target intent filters and dependency

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 
 android {
     namespace = "com.example.yank"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 37
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
